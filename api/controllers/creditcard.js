1768751d7226cab81974f4050b5b3ba75a29c977
clinton <hshbled@gmail.com>
2024-11-03 21:46:13 +0900

    2.3.0

--- api/controllers/creditcard.js
+++ api/controllers/creditcard.js
@@ -14,14 +14,17 @@ export const search = async (req, res, next) => {
 
 export const write = async (req, res, next) => {
     try {
-        const { date: dateString, price: priceString, people: peopleString, use } = req.body
+        let { date: dateString, price: priceString, people: peopleString, use, content } = req.body
         const date = sanitizeData(dateString, 'date')
         const price = Number(priceString)
         const people = Number(peopleString)
         const { name, email, cardNo } = req.user
-        const perPrice = Math.round(price /people)
+        const perPrice = Math.round(price / people)
+        if (!content) {
+            content = ''
+        }
         if (cardNo) {
-            const creditcard = CreditCard({date, name, email, cardNo, price, people, perPrice, use})
+            const creditcard = CreditCard({date, name, email, cardNo, price, people, perPrice, use, content})
             await creditcard.save()
             res.status(200).json(creditcard)
         } else {
@@ -34,12 +37,15 @@ export const write = async (req, res, next) => {
 
 export const update = async (req, res, next) => {
     try {
-        const { _id, date: dateString, price: priceString, people: peopleString, use } = req.body
+        let { _id, date: dateString, price: priceString, people: peopleString, use, content } = req.body
         const date = sanitizeData(dateString, 'date')
         const price = Number(priceString)
         const people = Number(peopleString)
-        const perPrice = Math.round(price /people)
-        await CreditCard.updateOne({_id}, {$set: {date, price, people, perPrice, use}}, {runValidators: true})
+        const perPrice = Math.round(price / people)
+        if (!content) {
+            content = ''
+        }
+        await CreditCard.updateOne({_id}, {$set: {date, price, people, perPrice, use, content}}, {runValidators: true})
         res.status(204).send()
     } catch (err) {
         next(err)
